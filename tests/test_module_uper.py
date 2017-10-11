d5d0692ec7437334c9de973f326267eb2dd1840d
ezor1 <ezor1@users.noreply.github.com>
2017-10-11 10:56:48 +0200

    Update test_module_uper.py

--- tests/test_module_uper.py
+++ tests/test_module_uper.py
@@ -6,15 +6,15 @@ def main():
   spec = asn1tools.compile_file('tests/files/Spec.asn', 'uper')
   
   #encode with types from two different modules specified in the same file Spec.asn
-  enc_1 = spec.encode('aSequence',{'priority:'High', 'src':1,'dst':2,'num':0,'length':256})
-  enc_2 = spec.encode('Seq_Mod_2',{'isvalide':True, 'name':'toto', 'ID':5)
+  enc_1 = spec.encode('ASequence',{'priority:'high', 'src':1,'dst':2,'num':0,'length':256})
+  enc_2 = spec.encode('SeqMod2',{'isvalide':True, 'name':'toto', 'ID':5)
 
-  print(enc_1)
-  print(enc_2)
+  print(enc_1,'hex')
+  print(enc_2,'hex')
 
 
   #decode
-  dec_1 = spec.decode('aSequence',enc_1)
+  dec_1 = spec.decode('ASequence',enc_1)
   dec_2 = spec.decode('SeqMod2',enc_2)
 
   print(dec_1)
