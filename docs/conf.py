2d3550e358753eb031db72461b87efb0dfd2c003
Erik Moqvist <erik.moqvist@gmail.com>
2018-01-02 00:47:31 +0100

    Copyright year 2018.

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'asn1tools'
-copyright = u'2017, Erik Moqvist'
+copyright = u'2017-2018, Erik Moqvist'
 author = u'Erik Moqvist'
 
 # The version info for the project you're documenting, acts as replacement for
