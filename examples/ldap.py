2dfa38705f7302e555d3a9fb5d896cdcaceb425f
Erik Moqvist <erik.moqvist@gmail.com>
2018-01-15 23:03:54 +0100

    LDAP example output improvements.

--- examples/ldap.py
+++ examples/ldap.py
@@ -5,15 +5,13 @@
 Example execution:
 
 $ ./ldap.py
-Sending LDAP bind request:
-
+Connecting to ldap.forumsys.com:389... done.
 {'messageID': 1,
  'protocolOp': {'bindRequest': {'authentication': {'simple': b'password'},
                                 'name': b'uid=tesla,dc=example,dc=com',
                                 'version': 3}}}
-
-Received LDAP bind response:
-
+Sending LDAP bind request to the server... done.
+Receiving LDAP bind response from the server... done.
 {'messageID': 1,
  'protocolOp': {'bindResponse': {'diagnosticMessage': bytearray(b''),
                                  'matchedDN': bytearray(b''),
@@ -35,12 +33,15 @@ RFC4511_ASN_PATH = os.path.join(SCRIPT_DIR,
                                 'files',
                                 'ietf',
                                 'rfc4511.asn')
-
+HOST = 'ldap.forumsys.com'
+PORT = 389
 db = asn1tools.compile_files(RFC4511_ASN_PATH)
 
 # Connect to the LDAP server.
 sock = socket.socket()
-sock.connect(('ldap.forumsys.com', 389))
+print('Connecting to {}:{}... '.format(HOST, PORT), end='')
+sock.connect((HOST, PORT))
+print('done.')
 
 # Encode the LDAP bind request and send it to the server.
 bind_request = {
@@ -58,22 +59,18 @@ bind_request = {
 
 encoded_bind_request = db.encode('LDAPMessage', bind_request)
 
-print('Sending LDAP bind request:')
-print()
 pprint(bind_request)
-print()
-
+print('Sending LDAP bind request to the server... ', end='')
 sock.sendall(encoded_bind_request)
+print('done.')
 
 # Receive the bind response, decode it, and print it.
+print('Receiving LDAP bind response from the server... ', end='')
 encoded_bind_response = sock.recv(2)
 length = db.decode_length(encoded_bind_response)
 encoded_bind_response += sock.recv(length - 2)
+print('done.')
 
 bind_response = db.decode('LDAPMessage', encoded_bind_response)
-
-print('Received LDAP bind response:')
-print()
 pprint(bind_response)
-
 sock.close()
