804ece741c3fb3e16c9ea143f29531082b4f9b44
Erik Moqvist <erik.moqvist@gmail.com>
2018-03-09 18:40:54 +0100

    Version 0.48.0.

--- asn1tools/__init__.py
+++ asn1tools/__init__.py
@@ -21,7 +21,7 @@ from .errors import CompileError
 
 
 __author__ = 'Erik Moqvist'
-__version__ = '0.47.0'
+__version__ = '0.48.0'
 
 
 def _decode_hexstring(codec_spec, asn1_spec, type_name, hexstring):
