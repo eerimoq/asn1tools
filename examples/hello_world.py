daf9baa5ba86a130f1ecc32b06c8ada54924e1cf
Erik Moqvist <erik.moqvist@gmail.com>
2018-06-19 19:29:39 +0200

    Hello world example output.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+"""The asn1tools hello world example.
+
+Example execution:
+
+$ ./hello_world.py
+Message: {'number': 2, 'text': 'Hi!'}
+Encoded: 010203486921
+Decoded: {'number': 2, 'text': 'Hi!'}
+$
+
+"""
+
 from __future__ import print_function
 from binascii import hexlify
 import asn1tools
