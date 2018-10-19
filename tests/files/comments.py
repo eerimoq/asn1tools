f542fd70493f915194e8e4223678e62f3d87e2c6
Erik Moqvist <erik.moqvist@gmail.com>
2018-10-19 08:34:37 +0200

    Added missing comments test file.

---
+++ tests/files/comments.py
@@ -0,0 +1,6 @@
+EXPECTED = {'Foo': {'extensibility-implied': False,
+         'imports': {},
+         'object-classes': {},
+         'object-sets': {},
+         'types': {},
+         'values': {}}}
