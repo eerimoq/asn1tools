af34f06afc6313047494e08c6feb5d47ed8b7f85
Erik Moqvist <erik.moqvist@gmail.com>
2018-12-16 08:38:55 +0100

    Some members in generated structs.

--- asn1tools/c_source/oer.py
+++ asn1tools/c_source/oer.py
@@ -3,10 +3,13 @@
 """
 
 from .utils import camel_to_snake_case
+from ..codecs import oer
+from ..errors import Error
 
 
 STRUCT_FMT = '''\
 struct {namespace}_{module_name_snake}_{type_name_snake}_t {{
+{members}
 }};
 '''
 
@@ -61,13 +64,59 @@ ssize_t {namespace}_{module_name_snake}_{type_name_snake}_decode(
 '''
 
 
-def _generate_struct(namespace, module_name, type_name):
+def _format_type_name(length, is_signed):
+    type_name = 'int{}_t'.format(length)
+
+    if not is_signed:
+        type_name = 'u' + type_name
+
+    return type_name
+
+
+def type_length(length):
+    if length <= 8:
+        return 8
+    elif length <= 16:
+        return 16
+    elif length <= 32:
+        return 32
+    else:
+        return 64
+
+
+def _format_integer(compiled_type):
+    if compiled_type.fmt is None:
+        raise Error('INTEGER not fixed size.')
+
+    type_name = _format_type_name(8 * compiled_type.length,
+                                  compiled_type.fmt[1].islower())
+
+    return '    {} value;'.format(type_name)
+
+
+def _format_sequence(compiled_type):
+    members = []
+
+    for member in compiled_type.root_members:
+        members.append('    bool {};'.format(member.name))
+
+    return members
+
+
+def _generate_struct(namespace, compiled_type, module_name, type_name):
     module_name_snake = camel_to_snake_case(module_name)
     type_name_snake = camel_to_snake_case(type_name)
+    members = []
+
+    if isinstance(compiled_type, oer.Integer):
+        members.append(_format_integer(compiled_type))
+    elif isinstance(compiled_type, oer.Sequence):
+        members.extend(_format_sequence(compiled_type))
 
     return STRUCT_FMT.format(namespace=namespace,
                              module_name_snake=module_name_snake,
-                             type_name_snake=type_name_snake)
+                             type_name_snake=type_name_snake,
+                             members='\n'.join(members))
 
 
 def _generate_declaration(namespace, module_name, type_name):
@@ -96,8 +145,9 @@ def generate(compiled, namespace):
     definitions = []
 
     for module_name, module in sorted(compiled.modules.items()):
-        for type_name, _ in sorted(module.items()):
+        for type_name, compiled_type in sorted(module.items()):
             struct = _generate_struct(namespace,
+                                      compiled_type.type,
                                       module_name,
                                       type_name)
             structs.append(struct)
