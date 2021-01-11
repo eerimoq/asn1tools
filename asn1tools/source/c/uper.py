a81dc3fd78dfdeda96247c7f0801e7669367b915
Florian Fetz <florian.fetz@googlemail.com>
2021-01-11 20:52:20 +0100

    Fixed generated uper C code for 24 bit integers

--- asn1tools/source/c/uper.py
+++ asn1tools/source/c/uper.py
@@ -125,9 +125,8 @@ class _Generator(Generator):
         suffix = type_name[:-2]
         location = self.location_inner()
 
-        if (type_.number_of_bits % 8) == 0 and checker.minimum in [0, -128, -32768,
-                                                                   -2147483648,
-                                                                   -9223372036854775808]:
+        if type_.number_of_bits in [8, 16, 32, 64] and \
+                checker.minimum in [0, -128, -32768, -2147483648, -9223372036854775808]:
             return (
                 [
                     'encoder_append_{}(encoder_p, src_p->{});'.format(
